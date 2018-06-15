d65497fb8fa5b7de5a9f4d72334dcefdb199541b
Kevin Coyner <kevin@rustybear.com>
2018-06-14 23:13:23 -0400

    Correctly writes userId entries to file

--- rfid.js
+++ rfid.js
@@ -1,7 +1,10 @@
 const http = require('http')
 const net = require('net')
 const url = require('url')
-const DateTime = require('luxon') 
+const fs = require('fs')
+
+const APPARATUS_ID = 'E4'
+const DEBUG = false
 
 // Create an HTTP tunneling proxy
 const proxy = http.createServer((req, res) => {
@@ -34,9 +37,10 @@ proxy.listen(1337, '127.0.0.1', () => {
   const req = http.request(options)
   req.end()
 
+  let usersObject = {users: []}
+
   req.on('connect', (res, socket, head) => {
     console.log('got connected!')
-
     // make a request over an HTTP tunnel
     socket.write('GET / HTTP/1.1\r\n' +
                  'Host: 10.10.100.100:8899\r\n' +
@@ -46,10 +50,39 @@ proxy.listen(1337, '127.0.0.1', () => {
       let rawInfo = JSON.parse(JSON.stringify(chunk))
 
       if (rawInfo.data.length === 16) {
+	// TODO: with many users present, rawInfo.data might be longer than 16.
+	// In that case, it will be either 16, 32, 48, etc.
+	// slice(12) will still work, but
+	// it would be better to take the rawInfo.data array, chunk into lengths of 16,
+	// and then slice(12) each of those chunks
+
+	// create a userId from the raw data i.e. 12-34-56-789
 	let userInfo = rawInfo.data.slice(12)
 	userId = userInfo.map(num => num + '-').join('').slice(0, -1)
-	console.log(userId)
+
+	let allUserIds = usersObject.users.map(user => user.userId)
+
+	// is userId already in userObject.userId?
+	if (allUserIds.includes(userId)) {
+	  // if true, then find the index position of the user and update lastTimeStamp  
+          let idx = allUserIds.indexOf(userId)
+	  usersObject.users[idx].lastTimeStamp = Date.now()
+	} else {
+	  // if false, then add the new user
+	  usersObject.users.push({
+	    'userId': userId,
+	    'firstTimeStamp': Date.now(),
+	    'lastTimeStamp': Date.now(),
+	    'apparatusId': APPARATUS_ID
+	  })
+	}
       }
+      if (DEBUG) { console.log('usersObject.users: ', usersObject.users) }
+
+      fs.writeFile('rfid-data.txt', JSON.stringify(usersObject.users), (err) => {
+	  if (err) throw err;
+	  console.log('The file has been saved!');
+      })
 
     })
     socket.on('end', () => {
